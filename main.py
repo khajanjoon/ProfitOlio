23117702abd4df38e716390d5b0f07c056c5ea83
Harsh Sinha <sinha.harshsep@gmail.com>
2024-06-19 12:56:05 +0530

    added icons

--- main.py
+++ main.py
@@ -719,7 +719,7 @@ if 'user_id' in st.session_state:
             selected = option_menu(
             menu_title = None,
             options = ['Portfolio', 'Charts', 'Indian Market', 'FinBot', 'P&L to Date', 'Stock Metrics', 'Price Predictor', 'Financial Statement','Widgets'],
-            icons = ['🍎', '💡', '🍊', '🫡', '📈', '₹', '🥳', '😇', '🤩'],
+            icons = ['briefcase', 'bar-chart-line', 'globe', 'robot', 'calendar-check', 'graph-up', 'calculator', 'file-earmark-text', 'puzzle-fill'],
             menu_icon = "cast",
             default_index = 0,
             orientation = "horizontal",
